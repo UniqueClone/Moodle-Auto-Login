f5ce7f4099080c63b64d726c09d7d65516731174
Ryan <lynchryan2@gmail.com>
2020-12-06 23:01:33 +0000

    Added auto-redirect to grades page
    The grades page has an error on login after auto-logout. This redirect now auto-fixes that error

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
-window.open("https://able.moodle.maynoothuniversity.ie/auth/oidc/", "_self");
+
+if(window.location.href == "https://able.moodle.maynoothuniversity.ie/login/index.php") {
+    window.open("https://able.moodle.maynoothuniversity.ie/auth/oidc/", "_self");
+}
+
+if (window.location.href == "https://able.moodle.maynoothuniversity.ie/grade/report/overview/index.php?id=1&userid=0") {
+    window.open("https://able.moodle.maynoothuniversity.ie/grade/report/overview/index.php", "_self");
+}
+
+console.log("Got here!");
